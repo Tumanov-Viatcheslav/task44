b654c43ccf3feba77ebf135ed3f40b467ad67181
Tumanov <tslavich@bk.ru>
2024-09-25 10:47:30 +0300

    Simplified

--- src/Arrows.java
+++ src/Arrows.java
@@ -17,20 +17,20 @@ public class Arrows {
     private static int countArrows(String line) {
         if (line.length() < 5)
             return 0;
-        int pos1 = 0, pos2 = 0, pos1tmp, pos2tmp, counter = 0;
-        pos1tmp = line.indexOf(">>-->", pos1);
-        pos2tmp = line.indexOf("<--<<", pos2);
-        while (pos1tmp != -1 || pos2tmp != -1) {
-            if (pos1tmp != -1) {
-                pos1 = pos1tmp + 1;
+        int pos1 = 0, pos2 = 0, counter = 0;
+        pos1 = line.indexOf(">>-->", pos1);
+        pos2 = line.indexOf("<--<<", pos2);
+        while (pos1 != -1 || pos2 != -1) {
+            if (pos1 != -1) {
+                pos1++;
                 counter++;
             }
-            if (pos2tmp != -1) {
-                pos2 = pos2tmp + 1;
+            if (pos2 != -1) {
+                pos2++;
                 counter++;
             }
-            pos1tmp = line.indexOf(">>-->", pos1);
-            pos2tmp = line.indexOf("<--<<", pos2);
+            pos1 = line.indexOf(">>-->", pos1);
+            pos2 = line.indexOf("<--<<", pos2);
         }
         return counter;
     }
